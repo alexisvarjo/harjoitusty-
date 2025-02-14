b27ed4e11a1cefa930c60b60e5e31bb050568d73
alexisvarjo <alexisvarj@gmail.com>
2025-02-14 18:09:52 +0200

    pari printtiä niin käyttäjä näkee mitä on tapahtumassa

--- src/main.cpp
+++ src/main.cpp
@@ -161,6 +161,7 @@ int main(int argc, char *argv[]) {
                 }
                 std::filesystem::remove(filename);
             }
+            std::cout << "Processed file: " << fp.string() << std::endl;
         }
         return 0;
     }
